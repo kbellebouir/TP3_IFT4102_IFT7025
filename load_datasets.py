6e8d7359a2e8af0d708c219830ef645e3967b185
Maxime Mainardi <maxime.mainardi.1@ulaval.ca>
2024-12-01 13:59:54 -0500

    fixed load_datasets

--- load_datasets.py
+++ load_datasets.py
@@ -31,16 +31,13 @@ def load_iris_dataset(train_ratio):
     random.seed(1)
 
     # Vous pouvez utiliser des valeurs numériques pour les différents types de classes, tel que :
-    conversion_labels = {'Iris-setosa': 0,'Iris-versicolor': 1, 'Iris-virginica': 2}
+    conversion_labels = {'Iris-setosa': 0,
+                         'Iris-versicolor': 1, 'Iris-virginica': 2}
 
     # Le fichier du dataset est dans le dossier datasets en attaché
-    with open('datasets/bezdekIris.data', 'r') as f:
-        dataset = np.loadtxt(
-            f,
-            delimiter=',',
-            dtype=float,
-            converters={4: lambda s: conversion_labels[s.decode('utf-8')]}
-        )
+    f = open('datasets/bezdekIris.data', 'r')
+
+    # TODO : le code ici pour lire le dataset
 
     # REMARQUE très importante :
     # remarquez bien comment les exemples sont ordonnés dans
@@ -49,18 +46,23 @@ def load_iris_dataset(train_ratio):
     # vous n'allez avoir aucun exemple du type Iris-virginica pour l'entrainement, pensez
     # donc à utiliser la fonction random.shuffle pour melanger les exemples du dataset avant de séparer
     # en train et test.
+    dataset = np.loadtxt(f, delimiter=',', dtype=float, converters={
+                         4: lambda s: conversion_labels[s]})
+    f.close()
+
     np.random.shuffle(dataset)
+
     X = dataset[:, :4]
     y = dataset[:, 4]
     y = y.astype(int)
     length = len(y)
     train_length = int(length * train_ratio)
-    test_length = length - train_length
+
     train = X[:train_length]
     train_labels = y[:train_length]
-    test = X[test_length:]
-    test_labels = y[test_length:]
-    f.close()
+
+    test = X[train_length:]
+    test_labels = y[train_length:]
 
     # Tres important : la fonction doit retourner 4 matrices (ou vecteurs) de type Numpy.
     return (train, train_labels, test, test_labels)
@@ -95,6 +97,7 @@ def load_wine_dataset(train_ratio):
     # Le fichier du dataset est dans le dossier datasets en attaché
     f = open('datasets/binary-winequality-white.csv', 'r')
 
+    # TODO : le code ici pour lire le dataset
     dataset = np.loadtxt(f, delimiter=',', dtype=float)
     random.shuffle(dataset)
     X = dataset[:, :11]
@@ -102,11 +105,10 @@ def load_wine_dataset(train_ratio):
     y = y.astype(int)
     length = len(y)
     train_length = int(length * train_ratio)
-    test_length = length - train_length
     train = X[:train_length]
     train_labels = y[:train_length]
-    test = X[test_length:]
-    test_labels = y[test_length:]
+    test = X[train_length:]
+    test_labels = y[train_length:]
     f.close()
 
     # La fonction doit retourner 4 structures de données de type Numpy.
@@ -136,18 +138,14 @@ def load_abalone_dataset(train_ratio):
         - test_labels : contient les étiquettes pour chaque exemple dans test, de telle sorte
           que : test_labels[i] est l'etiquette pour l'exemple test[i]
     """
+    f = open('datasets/abalone-intervalles.csv',
+             # La fonction doit retourner 4 matrices (ou vecteurs) de type Numpy.
+             'r')
     # Male : 0, Female : 1, Infant : 2
     conversion_sexe = {'M': 0, 'F': 1, 'I': 2}
 
-    with open('datasets/abalone-intervalles.csv', 'r') as f:
-        # Load the dataset, converting the first column using `conversion_sexe`
-        dataset = np.loadtxt(
-            f,
-            delimiter=',',
-            dtype=float,
-            converters={0: lambda s: conversion_sexe[s.decode('utf-8')]}
-        )
-        
+    dataset = np.loadtxt(f, delimiter=',', dtype=float, converters={
+                         0: lambda s: conversion_sexe[s]})
     random.shuffle(dataset)
     X = dataset[:, :8]
     # make the first column of X to be integers in X
@@ -156,10 +154,9 @@ def load_abalone_dataset(train_ratio):
     y = y.astype(int)
     length = len(y)
     train_length = int(length * train_ratio)
-    test_length = length - train_length
     train = X[:train_length]
     train_labels = y[:train_length]
-    test = X[test_length:]
-    test_labels = y[test_length:]
+    test = X[train_length:]
+    test_labels = y[train_length:]
     f.close()
     return (train, train_labels, test, test_labels)
